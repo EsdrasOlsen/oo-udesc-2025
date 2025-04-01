8b045d2714f64ed528ff884e87ae45024ba4a341
Esdras <esdraske@gmail.com>
2025-03-31 21:13:25 -0300

    Criação da Classe OlaMundo e classe de teste TesteOlaMundo

---
+++ src/TestaOlaMundo.java
@@ -0,0 +1,7 @@
+public class TestaOlaMundo {
+    public static void main(String[] args) {
+        //instanciando um objeto: nome da classe, nome do objeto, nome da classe
+        OlaMundo xyz = new OlaMundo();
+        xyz.imprimir("Esdras Olsen", 19);
+    }
+}
