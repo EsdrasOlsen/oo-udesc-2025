596ccc0b0b368ebc4f68384e38973c77b91b3393
Esdras <esdraske@gmail.com>
2025-03-31 21:44:42 -0300

    Criação de Scanner para ler dados informados pelo usuario

--- src/teste/TestaOlaMundo.java
+++ src/teste/TestaOlaMundo.java
@@ -1,9 +1,20 @@
 package teste;
 
+import java.util.Scanner;
+
 public class TestaOlaMundo {
     public static void main(String[] args) {
         //instanciando um objeto: nome da classe, nome do objeto, nome da classe
         OlaMundo xyz = new OlaMundo();
         xyz.imprimirDados();
+        xyz.imprimir("esdras", 19);
+
+    int valor;
+    Scanner lerDados = new Scanner(System.in);
+    System.out.print("Digite seu nome: ");
+    String nome = lerDados.nextLine();
+    System.out.print("Digite sua idade: ");
+    valor = lerDados.nextInt();
+    xyz.imprimir(nome, valor);
     }
 }
