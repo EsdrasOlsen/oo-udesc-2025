a37877fed3fd324e51fdc5e5beb4890519ad47b3
Esdras <esdraske@gmail.com>
2025-03-31 21:18:44 -0300

    Alteração de Nome e Idade da Classe OlaMundo

--- src/TestaOlaMundo.java
+++ src/TestaOlaMundo.java
@@ -2,6 +2,6 @@ public class TestaOlaMundo {
     public static void main(String[] args) {
         //instanciando um objeto: nome da classe, nome do objeto, nome da classe
         OlaMundo xyz = new OlaMundo();
-        xyz.imprimir("Esdras Olsen", 19);
+        xyz.imprimirDados();
     }
 }
